c453d3f9892b9cc1259e7fa6d7fd91e211d9f1f6
PotatoMochii <119642270+PotatoMochii@users.noreply.github.com>
2024-08-06 11:42:43 +0800

    third commit for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   experimental: {
       appDir: true,
-      css: true,
+      // css: true,
       serverComponentsExternalPackages: ["mongoose"],
       missingSuspenseWithCSRBailout: false
   },
